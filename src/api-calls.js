dd2ad09e482361f9c0baae6363aed4dda8f5b233
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-14 21:19:07 -0700

    Create the api-calls

---
+++ src/api-calls.js
@@ -0,0 +1,58 @@
+export const getAllPhotographers = async () => {
+    const url = `http://localhost:3001/api/v1/photographers`;
+    const response = await fetch(url)
+    if(!response.ok) {
+        throw new Error('Unable to retrieve photographers');
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export const getPhotographer = async userId => {
+    const url = `http://localhost:3001/api/v1/photographers/${userId}`
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error(`Unable to locate photographer id# ${userId}`);
+    }
+    
+    const data = await response.json();
+    return data;
+}
+
+export const sendNewPhotographer = async submittedPhotographer => {
+    const url = `http://localhost:3001/api/v1/photographers`
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(submittedPhotographer),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        const { message: errorMessage } = await response.json();
+        throw new Error(errorMessage)
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export const deletePhotographer = async userId => {
+    const url = `http://localhost:3001/api/v1/photographers/${userId}`
+    const options = {
+        method: 'DELETE'
+    }
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        throw new Error(`Unable to delete user id# ${userId}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
